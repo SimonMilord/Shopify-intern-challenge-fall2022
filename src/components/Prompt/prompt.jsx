d4d86d167d1f117b87799905139197827dd2dd43
Simon Milord <simon.milord@gmail.com>
2022-05-13 15:30:59 +0800

    added keyboard support to submit form

--- src/components/Prompt/prompt.jsx
+++ src/components/Prompt/prompt.jsx
@@ -14,6 +14,12 @@ export default function Prompt(props) {
     setPrompt("");
   };
 
+  const handleKeyPress = (e) => {
+    if (e.keyCode === 13) {
+      handleSubmit(e);
+    }
+  }
+
   return (
     <section className="prompt">
       <form className="prompt__form" onSubmit={handleSubmit}>
@@ -23,11 +29,20 @@ export default function Prompt(props) {
           placeholder="Enter a question for Marv here!"
           value={prompt}
           onChange={(e) => setPrompt(e.target.value)}
+          onKeyDown={handleKeyPress}
           name="input"
         ></textarea>
-        <button className="prompt__submit" type="submit">
-          Submit
-        </button>
+        <div className="prompt__buttons">
+          <button
+            className="prompt__submit prompt__submit--typed"
+            type="submit"
+          >
+            Submit
+          </button>
+          {/* <button className="prompt__submit prompt__submit--lazy" onClick={handleSubmit} name="lazy" type="submit">
+            Feeling lazy
+          </button> */}
+        </div>
       </form>
     </section>
   );
